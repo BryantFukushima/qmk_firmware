08a9017c768de247919f3cbb66af60eeaec868db
Bryant Fukushima <bmfukushima94@gmail.com>
2025-07-20 01:32:53 -0700

    update gaming keymap

--- keyboards/waterfowl/keymaps/bryant/keymap.c
+++ keyboards/waterfowl/keymaps/bryant/keymap.c
@@ -168,6 +168,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,
   KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,         KC_G,    KC_H,    KC_J,    KC_K,    KC_L,
   KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,         KC_B,    KC_N,    KC_M,    KC_NO,   KC_P,
-  TO(0), KC_LALT, KC_SPC, KC_ESC, KC_1,         KC_2, KC_ENT, KC_SPC, KC_BSPC, KC_3
+  TO(0), KC_LALT, KC_SPC, KC_ESC, KC_1,         KC_2, KC_ENT, KC_BSPC, KC_LGUI, KC_3
 ),
 };
